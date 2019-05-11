f57d4393b7ba944b0eb8473ac72d554e2db5440e
Martin Bielik <mx.bielik@gmail.com>
2019-05-11 12:00:27 +0200

    server setup

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const data = [
+  {
+    title: 'TODO',
+    id: '1',
+    tasks: [
+      {
+        id: '1',
+        title: 'Something',
+        notes: 'Harry Potter and the Chamber of Secrets',
+      },
+      {
+        id: '2',
+        title: 'Jurassic Park',
+      },
+    ],
+  },
+  {
+    title: 'Tmp',
+    id: '2',
+    tasks: [
+      {
+        id: '1',
+        title: 'Something',
+      },
+    ],
+  },
+]
+
+const typeDefs = gql`
+  type Link {
+    type: String
+    description: String
+    link: String
+  }
+  type Task {
+    id: String
+    etag: String
+    title: String
+    updated: String
+    selfLink: String
+    parent: String
+    position: String
+    notes: String
+    status: String
+    due: String
+    completed: String
+    deleted: String
+    hidden: String
+    links: [Link]
+  }
+  type TaskList {
+    kind: String
+    id: String
+    etag: String
+    title: String
+    selfLink: String
+    updated: String
+    tasks: [Task]
+  }
+  type Query {
+    taskLists: [TaskList]
+  }
+`
+
+const resolvers = {
+  Query: {
+    taskLists: () => data,
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})

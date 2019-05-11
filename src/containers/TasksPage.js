d2d2d8721457a7cdbd344915c5c143d8d17ef1e5
Martin Bielik <mx.bielik@gmail.com>
2019-05-11 19:04:54 +0200

    extracted checkbox

--- src/containers/TasksPage.js
+++ src/containers/TasksPage.js
@@ -1,8 +1,7 @@
 import { DragDropContext } from 'react-beautiful-dnd'
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
-import { makeStyles, withStyles } from '@material-ui/core/styles'
-import Checkbox from '@material-ui/core/Checkbox'
+import { makeStyles } from '@material-ui/core/styles'
 import DetailIcon from '@material-ui/icons/ChevronRight'
 import IconButton from '@material-ui/core/IconButton'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
@@ -13,6 +12,7 @@ import TextField from '@material-ui/core/TextField'
 import grey from '@material-ui/core/colors/grey'
 
 import { TaskDetailLink } from '../components/Link'
+import CompletedCheckbox from '../components/CompletedCheckbox'
 import DraggableList from '../components/DraggableList'
 import ListActionsToolbar from './app/tasks/ListActionsToolbar'
 import ListContextMenu from './app/tasks/ListContextMenu'
@@ -25,16 +25,7 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-const GreyCheckbox = withStyles({
-  root: {
-    color: grey[500],
-    '&$checked': {
-      color: grey[400],
-    },
-  },
-})(props => <Checkbox color="default" {...props} />)
-
-function CheckboxList() {
+function TasksPage() {
   const classes = useStyles()
   const [checked, setChecked] = React.useState([0])
 
@@ -107,7 +98,7 @@ function CheckboxList() {
                     children: (
                       <>
                         <ListItemIcon>
-                          <GreyCheckbox
+                          <CompletedCheckbox
                             edge="start"
                             checked={isChecked}
                             tabIndex={-1}
@@ -153,4 +144,4 @@ function CheckboxList() {
   )
 }
 
-export default CheckboxList
+export default TasksPage

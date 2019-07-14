f526bd0ee1bcd7226093b8c316267ed6a8a60f34
Martin Bielik <mx.bielik@gmail.com>
2019-07-14 16:28:22 +0200

    page not found

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -1,2 +1,10 @@
-// TODO: implement
-export default () => 'Page Not Found'
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+import PageTemplate from './app/PageTemplate'
+
+export default () => (
+  <PageTemplate>
+    <Typography variant="h4">Page not found</Typography>
+  </PageTemplate>
+)

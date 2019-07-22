8cda75e5a0e4b426045cf5662da8400821a87f3d
Martin Bielik <mx.bielik@gmail.com>
2019-07-22 11:05:59 +0200

    fixed login bug

--- src/containers/app/login/SignInButton.js
+++ src/containers/app/login/SignInButton.js
@@ -1,30 +1,13 @@
-import { Query } from 'react-apollo'
 import Button from '@material-ui/core/Button'
 import React from 'react'
 
-import { CURRENT_USER } from '../../../queries/loginQueries'
-import { LoginLink, TasksAppLink } from '../../../components/Link'
+import { LoginLink } from '../../../components/Link'
 
 function SignInButton() {
   return (
-    <Query query={CURRENT_USER}>
-      {({ loading, error, data }) => {
-        if (loading || error) return null
-        const isSignedIn = data.currentUser && data.currentUser.isSignedIn
-        if (isSignedIn) {
-          return (
-            <Button color="primary" component={TasksAppLink}>
-              Open tasks
-            </Button>
-          )
-        }
-        return (
-          <Button color="primary" component={LoginLink}>
-            Sign in
-          </Button>
-        )
-      }}
-    </Query>
+    <Button color="primary" component={LoginLink}>
+      Sign in
+    </Button>
   )
 }
 

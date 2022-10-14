c4700e32aa3c872b5cce8e0e7f41aaf152154f8a
Martin Bielik <mx.bielik@gmail.com>
2022-10-14 17:34:24 +0200

    linting, removed redundant files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import Button from '@mui/material/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -25,7 +22,7 @@ function App() {
         <Button variant="contained">Hello World</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

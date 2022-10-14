f93b6d2e511fa5c4c39fd793756234fed18f81d1
Martin Bielik <mx.bielik@gmail.com>
2022-10-14 17:29:12 +0200

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -17,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained">Hello World</Button>
       </header>
     </div>
   );

1bd526260c458fb4a02b334b87ed269b697f0841
Martin Bielik <mx.bielik@gmail.com>
2022-12-02 22:57:51 +0100

    legal pages

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,16 @@
+import { Box, Typography } from '@mui/material'
+
 export function ContactPage() {
-  return 'contact'
+  return (
+    <Box textAlign="center" minHeight="calc(100vh - 160px)" pt={10}>
+      <Typography variant="h2" pb={4}>
+        Contact
+      </Typography>
+      <Typography paragraph>
+        Have you experienced a bug or do you have an improvement idea? Contact
+        me with any kind of feecback at:
+      </Typography>
+      <Typography paragraph>mx.bielik@gmail.com</Typography>
+    </Box>
+  )
 }

532e3b2c3b404704bc05f2b82df7922fc72b68c5
Martin Bielik <mx.bielik@gmail.com>
2022-12-02 22:27:51 +0100

    google analytics, meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteEjsPlugin(viteConfig => ({
+      env: viteConfig.env,
+      isDev: viteConfig.mode === 'development',
+    })),
+  ],
   base: '/another-tasks',
 })
